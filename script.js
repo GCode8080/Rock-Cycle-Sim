eba5ff124e86103fd8beb0f0b6b89cd2d8d0d9a0
Kibblezmeow <86687524+Kibblezmeow@users.noreply.github.com>
2022-01-28 16:39:36 -0800

    asd

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+//---------------------variables-------------------------
 let heat = 0;
 let pressure = 0;
 let baseRock;
+let rockType;
+
+//---------------------Stuff-------------------------
+document.getElementById("heatOutput").value = " ";
+document.getElementById("pressureOutput").value = " ";
+document.getElementById("rockOutput").value = " ";
+
+//---------------------Functions-------------------------
+
+function makeRock() {
+    if (heat >= 1000) {
+        rockType = "Lava";
+        
+    } else if (heat >= 200 && pressure >= 300 && heat <= 900) {
+        rockType = "Metamorphic Rock"
+
+    } else {
+        rockType = "Stone";
+    }
+
+    
+
+
+    document.getElementById("rockOutput").value = rockType;
+}
+
+
+function addHeat() {
+    heat = heat + 100;
+    document.getElementById("heatOutput").value = heat + " °C";
+    makeRock();
+}
+
+function subtractHeat() {
+    heat = heat - 100;
+    document.getElementById("heatOutput").value = heat + " °C";
+    makeRock();
+}
+
+function addPressure() {
+    pressure = pressure + 100;
+    document.getElementById("pressureOutput").value = pressure + " MPa";
+    makeRock();
+}
+
+function subtractPressure() {
+    pressure = pressure - 100;
+    document.getElementById("pressureOutput").value = pressure + " MPa";
+    makeRock();
+}
